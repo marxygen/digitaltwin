9c5146871acd18b07dd6207e50b6e9fcef9c6261
Mark Ivchenko <marxygen@gmail.com>
2022-04-09 18:01:40 +0300

    Start adding entities

---
+++ digitaltwin/entities/api_entity.py
@@ -0,0 +1,43 @@
+class APIEntityMeta(type):
+    """APIEntityMeta is a metaclass providing necessary functionality for the APIEntity subclasses"""
+
+    def __new__(cls, name, bases, attrs):
+        klass = super(APIEntityMeta, cls).__new__(cls, name, bases, attrs)
+        # Set `API_PREFIX` to equal lowercase class name
+        klass.API_PREFIX = name.lower()
+        return klass
+
+
+class APIEntity(metaclass=APIEntityMeta):
+    """Any Entity that is to be managed by the API"""
+
+    CLIENT: "DigitalTwinClient"
+    READ_ONLY = False
+    API_PREFIX = None
+    JSON_EXEMPT = dict()
+
+    def to_json(self):
+        """Transform object to JSON representation"""
+        return {
+            key: value
+            for key, value in self.__dict__.items()
+            if not key.startswith("_")
+            and not callable(value)
+            and not key.isupper()
+            and key not in self.JSON_EXEMPT
+        }
+
+    @classmethod
+    def from_json(cls, json: dict) -> "APIEntity":
+        """Restore object from JSON representation
+
+        Args:
+            json (dict): JSON data
+
+        Returns:
+            APIEntity: APIEntity restored from JSON representation
+        """
+        entity = cls()
+        for key, value in json.items():
+            setattr(entity, key, value)
+        return entity
--- tests/test_api_calls.py
+++ tests/test_api_calls.py
@@ -3,11 +3,6 @@ from digitaltwin.exceptions import AccessForbiddenException
 import pytest
 
 
-def test_invalid_user_secret():
-    with pytest.raises(ValueError):
-        APICall(url="some/url", user_secret="haha")
-
-
 def test_access_forbidden():
     with pytest.raises(AccessForbiddenException):
         call = APICall(
